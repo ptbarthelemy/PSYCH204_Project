de7e6cc53d6b1e4a95337ba3a585affeb14dba7c
P. T. Barthelemy <p.t.barthelemy+git@gmail.com>
2012-12-02 12:06:46 -0800

    Added efficient way to calculate hypothesis space. Added paper template.

--- inference.py
+++ inference.py
@@ -32,6 +32,21 @@ def totalSet(s):
 		result = copy.deepcopy(newResult)
 	return result
 
+def f(mat, ns, nf):
+	if mat[ns][nf] is not None:
+		return mat[ns][nf]
+	elif nf == ns or nf == 0:
+		result = 1
+	else:
+		result = nf * f(mat, ns-1, nf) + f(mat, ns-1, nf-1)
+	mat[ns][nf] = result
+	return result
+
+
+def numRegexes(n):
+	mat = [[None for i in range(n)] for j in range(n)]
+	return sum(f(mat, n-1, i) for i in range(n))
+
 HSIZE = 100
 class Inference:
 	def __init__(self, numH, strings):
@@ -87,76 +102,10 @@ class Inference:
 		self.hSpace_.extend(newH)
 
 if __name__ == '__main__':
-	# generate all hypotheses
+	# print "%e"%numRegexes(25)
+
 	strings = ['abc', 'dbc', 'ebc', 'abc', 'dbc']
 	inf = Inference(HSIZE, strings)
-	inf.generateAll()
-	# for a, index in zip(inf.hSpace_, range(len(inf.hSpace_))):
-	# 	regex, prob = a
-	# 	regex.printGraph("output/hypothesis%d.png"%index)
-	inf.sortHypotheses()
-	for i in range(10):
-		print "Place", i, "probability", inf.hSpace_[i][1]
-		inf.hSpace_[i][0].printGraph("output/hypothesis-top%d.png"%i)
-
-	print "Worst probability", inf.hSpace_[-1][1]
-	inf.hSpace_[-1][0].printGraph("output/hypothesis-bottom.png")
-
-	# Test1: should recognize pattern in first 4 characters
-	# parameters: alpha = 2, permute_prob = 0.5
-	#strings = ['757-123', '757-134', '757-445', '757-915']
-	# strings = \
-	# ['757-123-2398'
-	# ,'757-134-1111'
-	# ,'757-445-2231'
-	# ,'757-915-5547'
-	# ,'(650) 853-9800'
-	# ,'(650) 325-8500'
-	# ,'(650) 326-0983'
-	# ,'(650) 493-9188'
-	# ,'(650) 853-3888'
-	# ,'(650) 328-8899'
-	# ,'(650) 323-7723']
-
-	# wildcard threshold: increasing variance means higher likelihood of wildcard
-	# workds with (0, 5)
-	# strings = ['abc', 'dbc'] # should not use a wildcard
-	# strings = ['abc', 'dbc', 'ebc'] # should use a wildcard
-	# strings = ['abc', 'dbc', 'ebc', 'fbc'] # should use a wildcard
-	# strings = ['abcdefg', 'dbcdefg', 'ebcdefg', 'fbcdefg'] # should use wildcard
-
-	# wildcard threshold: increasing the number of examples should decrease the likelihood of a wildcard
-	# works with (0, 5)
-	# strings = ['abc', 'dbc', 'ebc'] # should use a wildcard
-	# strings = ['abc', 'dbc', 'ebc', 'abc', 'dbc', 'ebc'] # should not use a wildcard
-	strings = ['abc', 'dbc', 'ebc', 'abc', 'dbc'] # should not use a wildcard
-
-	# kleene threshold: increasing the variance means higher likelihood of kleene
-	# works with (0, 2)
-	# strings = ['acb', 'acbb'] # should not use kleene
-	# strings = ['ab', 'abb', 'abbb'] # should use kleene
-	# strings = ['abc', 'abbc', 'abc', 'abbc', 'abc', 'abbc', 'abc', 'abbc'] # should not use kleene
-
-	# kleene threshold: increasing the number of examples should decrease the likelihood of kleene
-	# works with (0, 2)
-	# strings = ['ab', 'abb', 'abbb'] # should use kleene
-	# strings = ['ab', 'abb', 'abbb', 'ab', 'abb', 'abbb', 'ab', 'abb', 'abbb', 'ab', 'abb', 'abbb'] # should not use kleene
-
-	# strings = ['abcd', 'abce', 'bbce', 'bbcd']
-	# strings = ['abc', 'abbc', 'abc']
-	# strings = ['abc', 'adc', 'adce']
-	# strings = ['abcd', 'bbcd', 'dbcd']
-	# strings = ['abcd', 'bbcd', 'efgh'] # < we definitely won't be able to learn this
-	# strings = ['oee', 'oee', 'oe']
-	# strings = ['oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee', 'oeeeeeeeee'] # it takes way too long to NOT generalize
-	# strings = ['oeee', 'oeee', 'oe', 'oe', 'oe', 'oe', 'oeee'] # generalizes
-	# strings = ['oee', 'oee', 'oe', 'oe', 'oe', 'oe', 'oee'] # doesn't generalize
-	# strings = ['oeee', 'oeee', 'oe', 'oe', 'oe', 'oe', 'oeee', 'oeee', 'oeee', 'oe', 'oe', 'oe', 'oe', 'oeee']
-	inf = Inference(HSIZE, strings)
-	print "Base hypothesis probability", inf.baseHProb_
-	inf.baseH_.printGraph("output/inference/hbase.png")
-
-
 	for i in range(10): 
 		print "Iteration", i
 		inf.duplicateHypotheses(True)
@@ -164,10 +113,7 @@ if __name__ == '__main__':
 		print "  best cost", inf.hSpace_[0][1]
 		print "  worst cost", inf.hSpace_[-1][1]
 		inf.hSpace_[0][0].printGraph("output/inference/trial-%d-best.png" % i)
-		# inf.hSpace_[1][0].printGraph("output/inference/trial-%d-best2.png" % i)
-		# inf.hSpace_[2][0].printGraph("output/inference/trial-%d-best3.png" % i)
-		# inf.hSpace_[3][0].printGraph("output/inference/trial-%d-best4.png" % i)
-		# inf.hSpace_[4][0].printGraph("output/inference/trial-%d-best5.png" % i)
-		# inf.hSpace_[5][0].printGraph("output/inference/trial-%d-best6.png" % i)
 		inf.hSpace_[-1][0].printGraph("output/inference/trial-%d-worst.png" % i)
 		inf.cullHypotheses()
+
+
